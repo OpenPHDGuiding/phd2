d352f250d63f1065ef33f7bc5d70180d1136ea30
geoffreyh <geoffreyh@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2009-09-02 03:26:00 +0000

    update libindiclient to parse device messages

--- libindiclient/indigui.h
+++ libindiclient/indigui.h
@@ -10,7 +10,7 @@ extern void indigui_update_widget(struct indi_prop_t *iprop);
 extern void indigui_add_prop(struct indi_device_t *idev, const char *groupname, struct indi_prop_t *iprop);
 extern void *indigui_create_window(void);
 extern void indigui_show_dialog(void *data);
-
+extern void indigui_show_message(const char *message);
 #ifdef __cplusplus
 }
 #endif
