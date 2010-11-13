592272c1a57bf794c89b5e5e8dcb9475a8c8f607
geoffreyh <geoffreyh@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2010-11-13 13:23:45 +0000

    License typo and reformatting

--- trunk/phd.cpp
+++ trunk/phd.cpp
@@ -6,7 +6,7 @@
  *  Copyright (c) 2006, 2007, 2008, 2009, 2010 Craig Stark.
  *  All rights reserved.
  *
- *  This source code is distrubted under the following "BSD" license
+ *  This source code is distributed under the following "BSD" license
  *  Redistribution and use in source and binary forms, with or without 
  *  modification, are permitted provided that the following conditions are met:
  *    Redistributions of source code must retain the above copyright notice, 
