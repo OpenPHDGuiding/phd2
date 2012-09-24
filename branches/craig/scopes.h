fef242c9cdea3eea0f3d5ed61a8ece5595f15f02
celstark <celstark@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2012-09-24 15:10:27 +0000

    - Fixed bug in calculation of median filter for ROI mode - Removed Voyager mount option (they never finished their end) - Removed QSI interop from project

--- branches/craig/scopes.h
+++ branches/craig/scopes.h
@@ -45,14 +45,14 @@
 #define GUIDE_ASCOM
 #define GUIDE_GPUSB
 #define GUIDE_GPINT
-#define GUIDE_VOYAGER
+//#define GUIDE_VOYAGER
 
 
 #elif defined (__APPLE__)
 #define GUIDE_GPUSB
 #define GUIDE_GCUSBST4
 #define GUIDE_EQUINOX
-#define GUIDE_VOYAGER
+//#define GUIDE_VOYAGER
 //#define GUIDE_NEB
 #define GUIDE_EQMAC
 #endif /* apple */
