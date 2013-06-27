9794a8c72dfcdf63987aa0f336b70f33b3a2004e
bretm <bretm@boneheads.us>
2013-06-27 03:47:43 +0000

    Fix whitespace issues

--- guidinglog.cpp
+++ guidinglog.cpp
@@ -320,7 +320,7 @@ bool GuidingLog::StartGuiding()
         if (m_enabled)
         {
             assert(m_file.IsOpened());
-                
+
             m_file.Write("\n");
             m_file.Write("Guiding Begins at " + pFrame->m_guidingStarted.Format(_T("%Y-%m-%d %H:%M:%S")) + "\n");
 
