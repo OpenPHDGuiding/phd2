a7d7a2165e90a5d3c9eb0d5f12637018b06c95f4
bretm@boneheads.us <bretm@boneheads.us@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2013-07-02 04:20:22 +0000

    Fix typos in licenses pointed out during code review

--- trunk/parallelport_win32.cpp
+++ trunk/parallelport_win32.cpp
@@ -14,7 +14,7 @@
  *    Redistributions in binary form must reproduce the above copyright notice,
  *     this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *    Neither the name of Bret McKee, Dad Dog Development, nor the names of its
+ *    Neither the name of Bret McKee, Dad Dog Development,
  *     Craig Stark, Stark Labs nor the names of its
  *     contributors may be used to endorse or promote products derived from
  *     this software without specific prior written permission.
@@ -84,6 +84,8 @@ bool ParallelPortWin32::Connect(wxString portName)
         }
 
         m_portAddr = addr;
+
+        Debug.AddLine(wxString::Format("com port %s assigned address 0x%x", portName, m_portAddr));
     }
     catch (wxString Msg)
     {
