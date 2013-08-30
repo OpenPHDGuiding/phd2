32b7b085a9a67737f71ed40835e2c568511348cf
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2013-08-30 05:04:21 +0000

    Add Bruce W to about dialog

--- trunk/about_dialog.cpp
+++ trunk/about_dialog.cpp
@@ -68,12 +68,14 @@ wxDialog(pFrame, wxID_ANY, _T("About ") APPNAME, wxDefaultPosition, wxDefaultSiz
         "&nbsp;&nbsp;&nbsp;&nbsp;John Wainwright<br>"
         "&nbsp;&nbsp;&nbsp;&nbsp;Sylvain Girard<br>"
         "&nbsp;&nbsp;&nbsp;&nbsp;Andy Galasso<br>"
+        "&nbsp;&nbsp;&nbsp;&nbsp;Bruce Waddington<br>"
         "<br>"
         "Copyright 2006-2013 Craig Stark<br>"
         "Copyright 2009 Geoffrey Hausheer<br>"
         "Copyright 2012-2013 Bret McKee<br>"
         "Copyright 2013 Sylvain Girard<br>"
         "Copyright 2013 Andy Galasso<br>"
+        "Copyright 2013 Bruce Waddington<br>"
         "</body></html>", APPNAME, FULLVER));
     wxHtmlWindow *pHtml;
     pHtml = new wxHtmlWindow(this, ABOUT_LINK, wxDefaultPosition, wxSize(380, 400), wxHW_SCROLLBAR_AUTO);
