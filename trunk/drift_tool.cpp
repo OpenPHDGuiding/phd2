746829ed8317953513f66f4cb41f6fc79f41cc0b
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2013-10-23 21:14:19 +0000

    Mac tweaks to fix missing/broken UI elements (Fixes Issue 199)

--- trunk/drift_tool.cpp
+++ trunk/drift_tool.cpp
@@ -144,6 +144,9 @@ DriftToolWin::DriftToolWin()
     instrSizer->Add(m_bmp, 0, wxALIGN_CENTER_VERTICAL|wxFIXED_MINSIZE, 5);
 
     m_instructions = new wxStaticText(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(300,90), wxALIGN_LEFT|wxST_NO_AUTORESIZE);
+#ifdef __WXOSX__
+    m_instructions->SetFont(*wxSMALL_FONT);
+#endif
     m_instructions->Wrap(-1);
     instrSizer->Add(m_instructions, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
 
