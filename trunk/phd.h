52794b5437ab570ee041d3d17bf5dd7848d63ef8
bretm@boneheads.us <bretm@boneheads.us@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2013-11-18 01:05:29 +0000

    Changes for Beta3

--- trunk/phd.h
+++ trunk/phd.h
@@ -73,7 +73,7 @@
 #include <math.h>
 
 #define APPNAME _T("PHD Guiding")
-#define PHDVERSION _T("2.1.1")
+#define PHDVERSION _T("2.1.3")
 #define PHDSUBVER _T("a")
 #define FULLVER PHDVERSION PHDSUBVER
 
