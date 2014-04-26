e885b1c092bd1b9c7d7c57f038c441b5fcf3e735
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2014-04-26 15:35:58 +0000

    merge changes from trunk to branch

--- branches/andy/phd.h
+++ branches/andy/phd.h
@@ -74,7 +74,7 @@
 
 #define APPNAME _T("PHD Guiding")
 #define PHDVERSION _T("2.2.2")
-#define PHDSUBVER _T("c")
+#define PHDSUBVER _T("d")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
