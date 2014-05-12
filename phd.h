a1c26acba185a1a807437e2e5f7a15a806a66d2e
andy.galasso <andy.galasso@gmail.com>
2014-05-12 22:11:11 +0000

    bump sub-version to "e"

--- phd.h
+++ phd.h
@@ -69,7 +69,7 @@
 
 #define APPNAME _T("PHD Guiding")
 #define PHDVERSION _T("2.2.2")
-#define PHDSUBVER _T("d")
+#define PHDSUBVER _T("e")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
