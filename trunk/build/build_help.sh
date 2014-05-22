60caec44cbe86ebb80491a928c5a9e2f22318658
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2014-05-22 19:59:02 +0000

    add help file sources to repo   run build/build_help.sh to generate PHD2GuideHelp.zip from sources   once this is integrated into the build systems (Win, OSX, Linux) we can remove PHD2GuideHelp.zip from SVN

---
+++ trunk/build/build_help.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# usage: build_help.sh [-w]
+#
+#    build PHD2GuideHelp.zip from help source files
+#
+# Options:
+#    -w      also build help files for the web site, man.tar.gz
+#
+
+set -e
+
+do_web=
+if [ "$1" = "-w" ]; then
+    do_web=1
+fi
+
+build=$(cd $(dirname $0); /bin/pwd)
+top="$build/.."
+help="$top/help"
+
+TMP=/tmp/phd2help$$
+mkdir "$TMP"
+trap "rm -rf $TMP" 2 3 15
+
+cp -p "$help"/* "$TMP"/
+
+(
+    cd "$TMP"
+
+    # generate the hhk (index) file
+    "$build"/build_help_hhk
+
+    # rebuild the help zip file
+    rm -f "$top"/PHD2GuideHelp.zip
+    zip -r "$top"/PHD2GuideHelp.zip .
+
+    if [ -n "$do_web" ]; then
+        echo "building web pages..."
+
+        # generate the HTML table of contents page
+        "$build"/build_help_toc_html > index.html
+
+        # generate the online help files
+        tar cfz "$top"/man.tar.gz *.html *.htm *.png
+
+        echo "done"
+    fi
+)
+
+rm -rf "$TMP"
