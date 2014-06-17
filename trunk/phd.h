6ba6bf0afc90329f0db5e8bbdab34b61f873fe26
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2014-06-17 06:07:20 +0000

    bump version to 2.3.0rc1

--- trunk/phd.h
+++ trunk/phd.h
@@ -68,8 +68,8 @@
 #include <stdarg.h>
 
 #define APPNAME _T("PHD2 Guiding")
-#define PHDVERSION _T("2.2.2")
-#define PHDSUBVER _T("h")
+#define PHDVERSION _T("2.3.0")
+#define PHDSUBVER _T("rc1")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
