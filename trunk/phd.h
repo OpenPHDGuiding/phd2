2eee7d85ab0efc5c3ac31267cba3a6feb3434e64
andy.galasso@gmail.com <andy.galasso@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2015-01-07 04:19:43 +0000

    bump sub-version to "c"

--- trunk/phd.h
+++ trunk/phd.h
@@ -69,7 +69,7 @@
 
 #define APPNAME _T("PHD2 Guiding")
 #define PHDVERSION _T("2.4.1")
-#define PHDSUBVER _T("b")
+#define PHDSUBVER _T("c")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
