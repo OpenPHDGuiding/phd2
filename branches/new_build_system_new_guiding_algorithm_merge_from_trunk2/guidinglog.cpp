4f965de21d43ed6c899788514ddaf84f47b466c2
raffi.enficiaud@gmail.com <raffi.enficiaud@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2015-01-23 19:33:51 +0000

    protecting the logging from a null pointer access

--- branches/new_build_system_new_guiding_algorithm_merge_from_trunk2/guidinglog.cpp
+++ branches/new_build_system_new_guiding_algorithm_merge_from_trunk2/guidinglog.cpp
@@ -216,7 +216,7 @@ static wxString RotatorPosStr(void)
 static wxString PointingInfo(void)
 {
     double cur_ra, cur_dec, cur_st;
-    if (!pPointingSource->GetCoordinates(&cur_ra, &cur_dec, &cur_st))
+    if (pPointingSource && !pPointingSource->GetCoordinates(&cur_ra, &cur_dec, &cur_st))
     {
         return wxString::Format("Dec = %0.1f deg, Hour angle = %0.2f hr, Pier side = %s, Rotator pos = %s",
             cur_dec, HourAngle(cur_ra, cur_st), PierSideStr(pPointingSource->SideOfPier()), RotatorPosStr());
