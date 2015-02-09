ac4e65a38983c679467ebacdd9252dfee4a4b5ad
raffi.enficiaud@gmail.com <raffi.enficiaud@gmail.com@a4fd5c06-7e29-11de-b8ac-f508f1a2864c>
2015-02-09 13:31:28 +0000

    Merged revision(s) 1478-1499 from branches/new_build_system: Merged revision(s) 1479 from branches/new_build_system_gp_guiding_integration_MPGUID-56: getting rid of the options from the command line for OSX ........
    ........
    isolating the precompiled headers for win32 only
    ........
    Fixing the link issues on linux, and requiring the zlib to finish the link
    ........
    fixing an ambiguity on abs
    ........
    fixing the dependencies of openssag
    ........
    changing the name of the final executable (upper/lower case depending on the platforms)
    ........
    fix locale install
    ........
    Now the project name is PHD2
    
    ........

--- branches/new_build_system_gp_guiding_integration_MPGUID-56/thirdparty/thirdparty.cmake
+++ branches/new_build_system_gp_guiding_integration_MPGUID-56/thirdparty/thirdparty.cmake
@@ -256,6 +256,7 @@ elseif(UNIX)
     include_directories(${USB_pkg_INCLUDE_DIRS})
     set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${USB_pkg_LIBRARIES})
     set(USB_build FALSE)
+    set(openphd_libusb ${USB_pkg_LIBRARIES})
   else()
 
     # in case the library is not installed on the system (as I have on my machines)
@@ -285,19 +286,19 @@ endif()
 if(${USB_build})
   include_directories(${libusb_dir})
 
-  # libUSB compilation for OSX and Win32 (not Linux)
-  add_library(USB ${libUSB_SRC})
-  target_include_directories(USB PRIVATE ${${LIBUSB}_additional_include_dir})
-  target_compile_definitions(USB PUBLIC ${${LIBUSB}_additional_compile_definition})
+  # libUSB compilation if OSX or Win32 or not installed on Linux
+  add_library(openphd_libusb ${libUSB_SRC})
+  target_include_directories(openphd_libusb PRIVATE ${${LIBUSB}_additional_include_dir})
+  target_compile_definitions(openphd_libusb PUBLIC ${${LIBUSB}_additional_compile_definition})
 
   if(NOT WIN32)
-    target_compile_definitions(USB PRIVATE LIBUSB_DESCRIBE "")
+    target_compile_definitions(openphd_libusb PRIVATE LIBUSB_DESCRIBE "")
   else()
     # silencing the warnings on externals for win32
-    target_compile_definitions(USB PRIVATE _CRT_SECURE_NO_WARNINGS)
+    target_compile_definitions(openphd_libusb PRIVATE _CRT_SECURE_NO_WARNINGS)
   endif()
-  set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} USB)
-  set_property(TARGET USB PROPERTY FOLDER "Thirdparty/")
+  set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} openphd_libusb)
+  set_property(TARGET openphd_libusb PROPERTY FOLDER "Thirdparty/")
 endif()
 
 
@@ -727,7 +728,7 @@ if(APPLE)
     ${libopenssag_dir}/openssag.h
     )
   add_library(OpenSSAG ${libOPENSSAG_SRC})
-  target_link_libraries(OpenSSAG USB)
+  target_link_libraries(OpenSSAG openphd_libusb)
   target_include_directories(OpenSSAG PRIVATE ${thirdparty_dir}/${LIBOPENSSAG}/src)
   set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} OpenSSAG)
   set_property(TARGET OpenSSAG PROPERTY FOLDER "Thirdparty/")
@@ -769,7 +770,7 @@ if(UNIX AND NOT APPLE)
   if(NOT asiCamera2)
     message(FATAL_ERROR "Cannot find the asiCamera2 drivers")
   endif()
-  set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${asiCamera2} USB) # to clean: USB should also appear after these lib, otherwise some references are not found
+  set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${asiCamera2})
 
 
   # math library is needed, and should be one of the last things to link to here
@@ -781,6 +782,10 @@ if(UNIX AND NOT APPLE)
   find_package(INDI 0.9 REQUIRED)
   include_directories(${INDI_INCLUDE_DIR})
   set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${INDI_CLIENT_LIBRARIES} ${INDI_LIBRARIES})
+
+  # INDI depends on libz
+  find_package(ZLIB REQUIRED)
+  set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${ZLIB_LIBRARIES})
 
   
   # Nova
