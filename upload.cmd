647b04994800977d77cf839c1cfb35d840b8a3fc
Patrick Chevalley <pch@ap-i.net>
2015-09-20 09:35:28 +0200

    Use a putty saved session instead of pageant

--- upload.cmd
+++ upload.cmd
@@ -1,6 +1,11 @@
 @echo off
 
 rem This script is in use by the Windows buildslaves to upload the package to openphdguiding.org
+rem 
+rem Before to run this script you need to install putty and add it to PATH.
+rem As we cannot use pageant from the buildbot service we need to create 
+rem a saved session named phd2buildbot in putty using the same user as the service.
+rem Set the hostname, auto-login user name and private key in the session. 
 
 set exitcode=1
 
@@ -10,7 +15,7 @@ for /F "usebackq" %%f in (`dir phd2-*-win32.exe /O-D /B` ) do (
    echo cd upload
    echo put %%f
    echo quit
-   ) | psftp phd2buildbot@openphdguiding.org
+   ) | psftp phd2buildbot
    set exitcode=%errorlevel%
   ) 
   goto :end
