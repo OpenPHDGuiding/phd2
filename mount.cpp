4d9e883c009aa92e881ac2735e3ed0065d9f6082
Andy Galasso <andy.galasso@gmail.com>
2016-03-27 01:23:26 -0400

    fix bugus debug log entries related to coordinate transforms

--- mount.cpp
+++ mount.cpp
@@ -895,13 +895,13 @@ bool Mount::TransformCameraCoordinatesToMountCoordinates(const PHD_Point& camera
             sin(yAngle) * hyp
             );
 
-        Debug.AddLine("CameraToMount -- cameraTheta (%.2f) - m_xAngle (%.2f) = xAngle (%.2f = %.2f)",
-                cameraTheta, m_cal.xAngle, xAngle, norm_angle(xAngle));
-        Debug.AddLine("CameraToMount -- cameraTheta (%.2f) - (m_xAngle (%.2f) + m_yAngleError (%.2f)) = yAngle (%.2f = %.2f)",
-                cameraTheta, m_cal.xAngle, m_yAngleError, yAngle, norm_angle(yAngle));
-        Debug.AddLine("CameraToMount -- cameraX=%.2f cameraY=%.2f hyp=%.2f cameraTheta=%.2f mountX=%.2f mountY=%.2f, mountTheta=%.2f",
+        Debug.Write(wxString::Format("CameraToMount -- cameraTheta (%.2f) - m_xAngle (%.2f) = xAngle (%.2f = %.2f)\n",
+                cameraTheta, m_cal.xAngle, xAngle, norm_angle(xAngle)));
+        Debug.Write(wxString::Format("CameraToMount -- cameraTheta (%.2f) - (m_xAngle (%.2f) + m_yAngleError (%.2f)) = yAngle (%.2f = %.2f)\n",
+                cameraTheta, m_cal.xAngle, m_yAngleError, yAngle, norm_angle(yAngle)));
+        Debug.Write(wxString::Format("CameraToMount -- cameraX=%.2f cameraY=%.2f hyp=%.2f cameraTheta=%.2f mountX=%.2f mountY=%.2f, mountTheta=%.2f\n",
                 cameraVectorEndpoint.X, cameraVectorEndpoint.Y, hyp, cameraTheta, mountVectorEndpoint.X, mountVectorEndpoint.Y,
-                mountVectorEndpoint.Angle());
+                mountVectorEndpoint.Angle()));
     }
     catch (const wxString& Msg)
     {
