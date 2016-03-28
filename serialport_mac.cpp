6559b2ec10719f5bdb2782fb4f608489146b71be
Simon <simonct@gmail.com>
2016-03-28 19:36:29 +0100

    Merge Posix and Mac serial port code

--- serialport_mac.cpp
+++ serialport_mac.cpp
@@ -35,9 +35,6 @@
 #ifdef __APPLE__
 
 #include "phd.h"
-
-#include <fcntl.h>
-#include <sys/ioctl.h>
 #include <IOKit/serial/IOSerialKeys.h>
 
 static kern_return_t createSerialIterator(io_iterator_t *serialIterator)
@@ -85,19 +82,6 @@ static const char *getRegistryString(io_object_t sObj, const char *propName)
     return resultStr;
 }
 
-SerialPortMac::SerialPortMac(void)
-{
-    m_PortFID = 0;
-}
-
-SerialPortMac::~SerialPortMac(void)
-{
-    if (m_PortFID > 0) {
-        close(m_PortFID);
-        m_PortFID = 0;
-    }
-}
-
 wxArrayString SerialPortMac::GetSerialPortList(void)
 {
     wxArrayString ret;
@@ -117,178 +101,6 @@ wxArrayString SerialPortMac::GetSerialPortList(void)
     }
     
     return ret;
-}
-
-bool SerialPortMac::Connect(const wxString& portName, int baud, int dataBits, int stopBits, PARITY Parity, bool useRTS, bool useDTR)
-{
-    const char* path = portName.ToStdString().c_str();
-    
-    m_PortFID = open(path, O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if (m_PortFID == -1){
-        fprintf(stderr,"Failed to open device at %s - %s(%d).\n",path,strerror(errno),errno);
-        return true;
-    }
-    
-    if (ioctl(m_PortFID, TIOCEXCL) == -1){
-        fprintf(stderr,"Error setting TIOCEXCL on %s - %s(%d).\n",path,strerror(errno),errno);
-        close(m_PortFID); m_PortFID = 0;
-        return true;
-    }
-    
-    if (fcntl(m_PortFID, F_SETFL, 0) == -1){
-        fprintf(stderr,"Error clearing O_NONBLOCK %s - %s(%d).\n",path,strerror(errno),errno);
-        close(m_PortFID); m_PortFID = 0;
-        return true;
-    }
-
-    if (tcgetattr(m_PortFID, &m_originalAttrs) == -1){
-        fprintf(stderr,"Error getting tty attributes %s - %s(%d).\n",path,strerror(errno),errno);
-        close(m_PortFID); m_PortFID = 0;
-        return true;
-    }
-
-    struct termios attrs = m_originalAttrs;
-    
-    cfmakeraw(&attrs);
-    attrs.c_cc[VMIN] = 1;
-    attrs.c_cc[VTIME] = 10;
-
-    switch (baud) {
-        case 9600:
-            cfsetspeed(&attrs, B9600);
-            break;
-        default:
-            fprintf(stderr,"Unrecognised baud %d.\n",baud);
-            close(m_PortFID); m_PortFID = 0;
-            return true;
-    }
-
-    switch (dataBits) {
-        case 8:
-            attrs.c_cflag |= CS8;
-            break;
-        default:
-            fprintf(stderr,"Unrecognised dataBits %d.\n",dataBits);
-            close(m_PortFID); m_PortFID = 0;
-            return true;
-    }
-    
-    switch (Parity) {
-        case SerialPort::ParityNone:
-            attrs.c_cflag &= ~PARENB;
-            break;
-        default:
-            fprintf(stderr,"Unrecognised parity %d.\n",Parity);
-            close(m_PortFID); m_PortFID = 0;
-            return true;
-    }
-
-    if (stopBits > 1){
-        attrs.c_cflag |= CSTOPB;
-    }
-    else {
-        attrs.c_cflag &= ~CSTOPB;
-    }
-    
-    if (tcsetattr(m_PortFID,TCSANOW,&attrs) == -1){
-        printf("Error setting tty attributes %s - %s(%d).\n",path,strerror(errno),errno);
-        close(m_PortFID); m_PortFID = 0;
-        return true;
-    }
-
-    int handshake = 0;
-    if (useRTS){
-        handshake |= TIOCM_RTS;
-    }
-    if (useDTR){
-        handshake |= TIOCM_DTR;
-    }
-    if (ioctl(m_PortFID,TIOCMSET,&handshake) == -1){
-        printf("Error setting handshake %s - %s(%d).\n",path,strerror(errno),errno);
-        close(m_PortFID); m_PortFID = 0;
-        return true;
-    }
-
-    return false;
-}
-
-bool SerialPortMac::Disconnect(void) {
-    bool bError = false;
-    
-    if (m_PortFID > 0) {
-        if (tcdrain(m_PortFID) == -1){
-            fprintf(stderr,"Error waiting for drain - %s(%d).\n",strerror(errno), errno);
-        }
-        if (tcsetattr(m_PortFID, TCSANOW, &m_originalAttrs) == -1){
-            fprintf(stderr,"Error resetting tty attributes - %s(%d).\n",strerror(errno),errno);
-        }
-        if (close(m_PortFID)){
-            fprintf(stderr,"Error closing port - %s(%d).\n",strerror(errno),errno);
-            bError = true;
-        }
-        m_PortFID = 0;
-    }
-    return bError;
-}
-
-bool SerialPortMac::SetReceiveTimeout(int timeoutMs) {
-    bool bError = false;
-    
-    try {
-        struct termios  options;
-        if (tcgetattr(m_PortFID, &options) == -1) {
-            throw ERROR_INFO("SerialPortMac: Unable to get port attributes");
-        }
-        options.c_cc[VMIN] = 1;
-        options.c_cc[VTIME] = timeoutMs / 10;
-        if (tcsetattr(m_PortFID, TCSANOW, &options) == -1)
-            throw ERROR_INFO("SerialPortMac: Unable to set port attributes");
-    }
-    catch (wxString Msg) {
-        POSSIBLY_UNUSED(Msg);
-        bError = true;
-    }
-    
-    return bError;
-}
-
-bool SerialPortMac::Send(const unsigned char *pData, unsigned count)
-{
-    if (m_PortFID < 1) {
-        return true;
-    }
-    
-    const ssize_t writeCount = write(m_PortFID, pData, count); // todo; retry ?
-    return !(writeCount == count);
-}
-
-bool SerialPortMac::Receive(unsigned char *pData, unsigned count)
-{
-    if (m_PortFID < 1) {
-        return true;
-    }
-    
-    do {
-        const ssize_t readCount = read(m_PortFID, pData, count);
-        if (readCount == -1){
-            fprintf(stderr,"Error writing to port - %s(%d).\n",strerror(errno),errno);
-            return true;
-        }
-        count -= readCount;
-        pData += readCount;
-    } while(count > 0);
-    
-    return !(count == 0);
-}
-
-bool SerialPortMac::SetRTS(bool asserted)
-{
-    return true;
-}
-
-bool SerialPortMac::SetDTR(bool asserted)
-{
-    return true;
 }
 
 #endif // _APPLE_
--- serialport_mac.h
+++ serialport_mac.h
@@ -36,30 +36,13 @@
 #if !defined(SERIALPORT_MAC_H_INCLUDED) && defined (__APPLE__)
 #define SERIALPORT_MAC_H_INCLUDED
 
-#include <termios.h>
+#include "serialport_posix.h"
 
-class SerialPortMac : public SerialPort
+class SerialPortMac : public SerialPortPosix
 {
-    int m_PortFID;
-    struct termios m_originalAttrs;
-    
 public:
 
     wxArrayString GetSerialPortList(void);
-
-    SerialPortMac(void);
-    virtual ~SerialPortMac(void);
-
-    virtual bool Connect(const wxString& portName, int baud, int dataBits, int stopBits, PARITY Parity, bool useRTS, bool useDTR);
-    virtual bool Disconnect(void);
-
-    virtual bool Send(const unsigned char *pData, unsigned count);
-
-    virtual bool SetReceiveTimeout(int timeoutMs);
-    virtual bool Receive(unsigned char *pData, unsigned count);
-    
-    virtual bool SetRTS(bool asserted);
-    virtual bool SetDTR(bool asserted);
 };
 
 #endif  // SERIALPORT_MAC_H_INCLUDED
