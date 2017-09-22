984f89c0dac0db7c84122b7450da3bfeb0626c5b
bwdev01 <bw_msg01@earthlink.net>
2017-09-21 17:54:16 -0700

    Fix bugs in calibration review and calibration sanity check when Dec guiding is disabled

--- calreview_dialog.cpp
+++ calreview_dialog.cpp
@@ -624,8 +624,8 @@ void CalSanityDialog::CreateDataGrids(wxPanel* parentPanel, wxSizer* parentHSize
     wxString raSteps = wxString::Format("%d", m_calDetails.raStepCount);
     wxString decSteps = wxString::Format("%d", m_calDetails.decStepCount);
     wxString oldAngleDelta;
-    double newRARate = m_newParams.xRate * 1000;                          // px per sec for UI purposes
-    double newDecRate = m_newParams.yRate * 1000;
+    double newRARate = m_newParams.xRate;
+    double newDecRate = m_newParams.yRate;
     double imageScale = m_calDetails.imageScale;
     bool oldValid = m_oldParams.isValid;
 
@@ -697,7 +697,7 @@ void CalSanityDialog::CreateDataGrids(wxPanel* parentPanel, wxSizer* parentHSize
         {
             pGrid->SetCellValue(row, col++, _("This declination rate:"));
             if (newDecRate != CALIBRATION_RATE_UNCALIBRATED)
-                pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newDecRate * imageScale, newDecRate));
+                pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newDecRate * 1000 * imageScale, newDecRate * 1000));
             else
                 pGrid->SetCellValue(row, col++, NA_STR);
             pGrid->SetCellValue(row, col++, _("Previous declination rate:"));
@@ -711,10 +711,10 @@ void CalSanityDialog::CreateDataGrids(wxPanel* parentPanel, wxSizer* parentHSize
         else
         {
             pGrid->SetCellValue(row, col++, _("RA rate:"));
-            pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newRARate * imageScale, newRARate));
+            pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newRARate * 1000 * imageScale, newRARate * 1000));
             pGrid->SetCellValue(row, col++, _("Declination rate:"));
             if (newDecRate != CALIBRATION_RATE_UNCALIBRATED)
-                pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newDecRate * imageScale, newDecRate));
+                pGrid->SetCellValue(row, col++, wxString::Format(_("%0.3f a-s/sec\n%0.3f px/sec"), newDecRate * 1000 *  imageScale, newDecRate * 1000));
             else
                 pGrid->SetCellValue(row, col++, NA_STR);
             if (m_issue == CI_Rates)
--- scope.cpp
+++ scope.cpp
@@ -793,18 +793,18 @@ void Scope::SanityCheckCalibration(const Calibration& oldCal, const CalibrationD
         if (m_lastCalibrationIssue == CI_None && oldCal.isValid &&
             fabs(oldDetails.imageScale - newDetails.imageScale) < 0.1 && (fabs(degrees(oldCal.xAngle - newCal.xAngle)) < 5.0))
         {
-            double newDecRate = newCal.yRate;
-            if (newDecRate != 0.)
+            if (newCal.yRate != CALIBRATION_RATE_UNCALIBRATED && oldCal.yRate != CALIBRATION_RATE_UNCALIBRATED)
             {
-                if (fabs(1.0 - (oldCal.yRate / newDecRate)) > CAL_ALERT_DECRATE_DIFFERENCE)
+                double newDecRate = newCal.yRate;
+                if (newDecRate != 0.)
                 {
-                    m_lastCalibrationIssue = CI_Different;
-                    detailInfo = wxString::Format("Current/previous Dec rate ratio is %0.3f", oldCal.yRate / newDecRate);
+                    if (fabs(1.0 - (oldCal.yRate / newDecRate)) > CAL_ALERT_DECRATE_DIFFERENCE)
+                    {
+                        m_lastCalibrationIssue = CI_Different;
+                        detailInfo = wxString::Format("Current/previous Dec rate ratio is %0.3f", oldCal.yRate / newDecRate);
+                    }
                 }
             }
-            else
-                if (oldCal.yRate != 0.)                   // Might have had Dec guiding disabled
-                    m_lastCalibrationIssue = CI_Different;
         }
     }
 
@@ -881,6 +881,8 @@ bool Scope::BeginCalibration(const PHD_Point& currentLocation)
         m_calibrationState = CALIBRATION_STATE_GO_WEST;
         m_calibrationDetails.raSteps.clear();
         m_calibrationDetails.decSteps.clear();
+        m_raSteps = 0;
+        m_decSteps = 0;
         m_calibrationDetails.lastIssue = CI_None;
     }
     catch (const wxString& Msg)
