426decfa7dae4df7af4bd89bde231caae4233d64
Ken Self <ken.kgself@gmail.com>
2017-10-30 22:28:34 +1100

    StaticPa use hypot function

--- staticpa_toolwin.cpp
+++ staticpa_toolwin.cpp
@@ -697,7 +697,7 @@ void StaticPaToolWin::CalcRotationCentre(void)
         radiff = norm_angle(radians((m_raPos[0] - m_raPos[1])*15.0*m_hemi));
 
         theta2 = radiff / 2.0; // Half the image rotation for midpoint of chord
-        double lenchord = sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
+        double lenchord = hypot((x1 - x2), (y1 - y2));
         cr = fabs(lenchord / 2.0 / sin(theta2));
         double lenbase = fabs(cr*cos(theta2));
         // Calculate the slope of the chord in pixels
@@ -724,7 +724,7 @@ void StaticPaToolWin::CalcRotationCentre(void)
     Debug.AddLine(wxString::Format("StaticPA CalcCoR: W:H:scale:angle %d: %d: %.1f %.1f", xpx, ypx, scalefactor, m_camAngle));
 
     // Distance and angle of CoR from centre of sensor
-    double cor_r = sqrt(pow(xpx / 2 - cx, 2) + pow(ypx / 2 - cy, 2));
+    double cor_r = hypot(xpx / 2 - cx, ypx / 2 - cy);
     double cor_a = degrees(atan2((ypx / 2 - cy), (xpx / 2 - cx)));
     double rarot = -m_camAngle;
     // Cone and Dec components of CoR vector
@@ -768,7 +768,7 @@ void StaticPaToolWin::CalcAdjustments(void)
     // Let harot =  camera rotation from Alt axis
     // Alt axis is at HA+90
     // This is camera rotation from RA minus(?) LST angle 
-    double    hcor_r = sqrt(pow(xt - xs, 2) + pow(yt - ys, 2)); //xt,yt: target, xs,ys: measured
+    double    hcor_r = hypot((xt - xs), (yt - ys)); //xt,yt: target, xs,ys: measured
     double    hcor_a = degrees(atan2((yt - ys), (xt - xs)));
     double ra_hrs, dec_deg, st_hrs, ha_deg;
     ha_deg = m_ha;
@@ -964,7 +964,7 @@ void StaticPaToolWin::PaintHelper(wxAutoBufferedPaintDCBase& dc, double scale)
     {
         stardeg = PHD_Point(m_poleStars->at(is).ra, m_poleStars->at(is).dec);
         starpx = Radec2Px(stardeg);
-        radpx = sqrt(pow(starpx.X, 2) + pow(starpx.Y, 2));
+        radpx = hypot(starpx.X, starpx.Y);
         wxColor line_color = (is == m_refStar) ? wxColor(0, intens, 0) : wxColor(intens, intens, 0);
         dc.SetPen(wxPen(line_color, 1, wxPENSTYLE_DOT));
         if (m_drawOrbit)
@@ -1095,7 +1095,7 @@ bool StaticPaToolWin::RotateMount()
             // And prevtheta is how we have already moved;
             // Recalculate the offset based on the actual movement;
             // CAUTION: This might end up in an endless loop. 
-            double actpix = sqrt(pow(m_pxPos[1].X - m_pxPos[0].X, 2) + pow(m_pxPos[1].Y - m_pxPos[0].Y, 2));
+            double actpix = hypot((m_pxPos[1].X - m_pxPos[0].X), (m_pxPos[1].Y - m_pxPos[0].Y));
             double actsec = actpix * m_pxScale;
             double actoffsetdeg = 90 - degrees(acos(actsec / 3600 / m_reqRot));
             Debug.AddLine(wxString::Format("StaticPA: Star Pos#2 actpix=%.1f actsec=%.1f m_pxScale=%.1f", actpix, actsec, m_pxScale));
