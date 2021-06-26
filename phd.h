933a8e80cb9b459ba6ca1bd6daaf5ecf674cd468
Andy Galasso <andy.galasso@gmail.com>
2021-06-26 15:07:31 -0400

    bump rev to 2.6.10

--- phd.h
+++ phd.h
@@ -78,8 +78,8 @@
 #include <stdarg.h>
 
 #define APPNAME _T("PHD2 Guiding")
-#define PHDVERSION _T("2.6.9")
-#define PHDSUBVER _T("dev5")
+#define PHDVERSION _T("2.6.10")
+#define PHDSUBVER _T("")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
