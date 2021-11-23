0aaf23b956ce920dd55618ce0f2f9a71479439b1
bwdev01 <bw_msg01@earthlink.net>
2021-11-23 12:42:18 -0800

    Remove unneeded code in initialization of MinHFD UI control

--- guider_multistar.cpp
+++ guider_multistar.cpp
@@ -1444,10 +1444,7 @@ void GuiderMultiStarConfigDialogCtrlSet::LoadValues()
     m_pMassChangeThreshold->Enable(starMassEnabled);
     m_pMassChangeThreshold->SetValue(100.0 * m_pGuiderMultiStar->GetMassChangeThreshold());
     m_pSearchRegion->SetValue(m_pGuiderMultiStar->GetSearchRegion());
-    double minHFD = m_pGuiderMultiStar->GetMinStarHFD();
-    if (minHFD < 1.0)
-        minHFD = 1.5;
-    m_MinHFD->SetValue(minHFD);
+    m_MinHFD->SetValue(m_pGuiderMultiStar->GetMinStarHFD());
     m_MinSNR->SetValue(m_pGuiderMultiStar->getMinStarSNR());
     m_autoSelDownsample->SetSelection(m_pGuiderMultiStar->GetAutoSelDownsample());
     m_pBeepForLostStarCtrl->SetValue(pFrame->GetBeepForLostStar());
