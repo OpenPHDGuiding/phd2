595e018a22297fae84c81c02ad165786ea33b7ac
moonboot <nielsrackwitz@googlemail.com>
2024-02-11 14:00:04 -0500

    Set  LIBNOVA  when using bundled Indi
    Also set LIBNOVA compiler option for compiling indi_scope.cpp when using bundled Indi

--- thirdparty/thirdparty.cmake
+++ thirdparty/thirdparty.cmake
@@ -634,6 +634,7 @@ else()
     else()
       find_library(LIBNOVA REQUIRED NAMES nova)
       set(PHD_LINK_EXTERNAL ${PHD_LINK_EXTERNAL} ${LIBNOVA} z)
+      add_definitions("-DLIBNOVA")
     endif()
     ## Define LIBNOVA when building Indi from source.
     add_definitions("-DLIBNOVA")
