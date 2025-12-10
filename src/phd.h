03374a8385f4f3a26c00858ffddbe94ce43f447f
Andy Galasso <andy.galasso@gmail.com>
2025-12-09 23:46:14 -0500

    bump rev to 2.6.14

--- src/phd.h
+++ src/phd.h
@@ -78,8 +78,8 @@
 #include <stdarg.h>
 
 #define APPNAME _T("PHD2 Guiding")
-#define PHDVERSION _T("2.6.13")
-#define PHDSUBVER _T("dev8")
+#define PHDVERSION _T("2.6.14")
+#define PHDSUBVER _T("")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined(__WINDOWS__)
