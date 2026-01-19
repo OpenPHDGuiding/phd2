4ba051d76a880fbccd09ba6ad7e89c04f7a6d583
bwdev01 <bw_msg01@earthlink.net>
2026-01-18 21:20:26 -0800

    Update src/profile_wizard.cpp
    Co-authored-by: Andy Galasso <andy.galasso@gmail.com>

--- src/profile_wizard.cpp
+++ src/profile_wizard.cpp
@@ -497,7 +497,7 @@ void ProfileWizard::ShowHelp(DialogState state)
         break;
     case STATE_ROTATOR:
         hText =
-            _("If you have a rotator positioned in front of the guide camera or OAG, you can select it here. This will allow "
+            _("If you have a rotator device that rotates the guide camera or OAG, you can select it here. This will allow "
               "PHD2 to automatically adjust calibration when the rotator "
               "is moved. Otherwise, any change in rotator position will require a re-calibration in PHD2. PHD2 NEVER "
               "sets options in the rotator software or changes the rotator position.");
