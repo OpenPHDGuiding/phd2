f657c40b1437841d8a88fd84941838e20b31daef
mikefsq <162642838+mikefsq@users.noreply.github.com>
2026-07-10 08:45:48 -0700

    formatting

--- src/cameras.h
+++ src/cameras.h
@@ -193,7 +193,7 @@
 # endif
 
 // ASCOM Alpaca camera backend (REST/JSON over libcurl)
-#define ALPACA_CAMERA
+# define ALPACA_CAMERA
 
 // Currently unused
 // #define NEB_SBIG   // This is for an on-hold project that would get the guide chip data from an SBIG connected in Neb
